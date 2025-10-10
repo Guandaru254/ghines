cca8f8980b8865d33ae38c95936f266b42f8d005
Guandaru254 <leroykim82@gmail.com>
2025-10-10 10:20:47 +0300

    Add files via upload

--- components/BlogList/BlogList.js
+++ components/BlogList/BlogList.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// 🛑 FIX 1: Use Next.js's built-in Link component
 import Link from 'next/link'; 
-import moment from 'moment'; // Required for date formatting
+import moment from 'moment'; 
 
 const BlogList = (props) => {
     
@@ -12,23 +11,31 @@ const BlogList = (props) => {
     return (
         <section className="blog-pg-section section-padding undefined">
             <div className="container">
-                <div className="row justify-content-center"> 
-                    {props.posts.map((post) => {
+                <div className="row"> 
+                    {props.posts.map((post, index) => {
                         // Date formatting using moment
                         const date = moment(post.PublishedDate);
-                        const day = date.format('DD'); // e.g., '01'
-                        const month = date.format('MMM').toUpperCase(); // e.g., 'OCT'
+                        const day = date.format('DD'); 
+                        const month = date.format('MMM').toUpperCase(); 
                         
                         // Construct the post link using the Slug
                         const postLink = `/blog-single/${post.Slug}`;
+                        
+                        // 🎯 NEW LOGIC: Apply different column sizes based on index
+                        // Posts 1 & 2 (index 0 and 1) get half width (6/12 columns) to be side-by-side.
+                        // All other posts (index 2 onwards) get full width (12/12 columns) for the 'single one' feature.
+                        const columnClass = index < 2 
+                            ? "col col-lg-6 col-md-12 col-sm-12 col-12" // Top two posts side-by-side
+                            : "col col-lg-12 col-md-12 col-sm-12 col-12"; // All posts 3+ are full width features
+
 
                         return (
-                            <div className="col col-lg-6 col-md-6 col-sm-12 col-12" key={post.id}>
+                            // Use the dynamically determined columnClass
+                            <div className={columnClass} key={post.id}>
                                 <div className="blog-item h-100 shadow-sm">
                                     {/* Conditionally render image/thumb if URL exists */}
                                     {post.Image?.url && (
                                         <div className="blog-img">
-                                            {/* 🛑 FIX 1: Use Next.js Link syntax */}
                                             <Link href={postLink}>
                                                 <img 
                                                     src={post.Image.url} 
@@ -40,7 +47,6 @@ const BlogList = (props) => {
 
                                     <div className="blog-content">
                                         <div className="date-overlay">
-                                            {/* 🎯 FIX for Vertical Stacking: Use separate divs. */}
                                             <div className="month-display">{month}</div>
                                             <div className="day-display">{day}</div>
                                         </div>
@@ -56,14 +62,12 @@ const BlogList = (props) => {
                                             </ul>
                                         </div>
 
-                                        {/* 🛑 FIX 1: Use Next.js Link syntax */}
                                         <h3>
                                             <Link href={postLink}>{post.Title}</Link>
                                         </h3>
                                         <p>{post.Description}</p>
 
                                         <div className="read-more">
-                                            {/* 🛑 FIX 1: Use Next.js Link syntax */}
                                             <Link href={postLink}>READ MORE...</Link>
                                         </div>
                                     </div>
